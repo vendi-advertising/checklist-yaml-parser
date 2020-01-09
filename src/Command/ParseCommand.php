c5769cdae25b96469264e4f6ac2c98ca763a669a
Chris Haas <chris@vendiadvertising.com>
2020-01-09 16:56:52 -0600

    First commit

---
+++ src/Command/ParseCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class ParseCommand extends Command
+{
+    protected static $defaultName = 'app:parse';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Parse the sample YAML file')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $arg1 = $input->getArgument('arg1');
+
+        if ($arg1) {
+            $io->note(sprintf('You passed an argument: %s', $arg1));
+        }
+
+        if ($input->getOption('option1')) {
+            // ...
+        }
+
+        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+
+        return 0;
+    }
+}
